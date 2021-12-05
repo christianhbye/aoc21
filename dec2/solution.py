fa996d5b3b4e63b9f62a4ac7ef5b703e32ae406a
Christian Hellum Bye <christian.hellum.bye@gmail.com>
2021-12-05 12:44:35 -0800

    complete pt 2

--- dec2/solution.py
+++ dec2/solution.py
@@ -1,12 +1,25 @@
 import numpy as np
 
-pos=np.array([0, 0])
-ops={'forward': (1, 0), 'down': (0, 1), 'up': (0, -1)}
-with open('input.txt', 'r') as f:
-    for i, l in enumerate(f): 
-        d=l.split(' ')
-        op=ops[d[0]]
-        factor=int(d[1].split('\n')[0])   
-        pos+=np.array(op)*factor  
-print(pos)
-print(np.product(pos))
+def run(part, pos, aim, ops):
+    with open('input.txt', 'r') as f:
+        for l in f: 
+            d=l.split(' ')
+            op=ops[d[0]]
+            factor=int(d[1].split('\n')[0])   
+            tup=np.array(op)*factor
+            if part==1:
+                pos+=tup
+            elif part==2:
+                pos[0]+=tup[0]
+                aim+=tup[1]
+                pos[1]+=tup[0]*aim
+    return pos
+
+if __name__=='__main__':
+    ops={'forward': (1, 0), 'down': (0, 1), 'up': (0, -1)}
+    for i in range(1, 3):
+        with open('output.txt', 'a+') as f:
+            f.write('Part {}: \n'.format(i))
+            pos_out=run(i, np.zeros(2), 0, ops)
+            f.write(str(pos_out))
+            f.write('\n {} \n'.format(np.product(pos_out)))
