53b1def5b30df13016852a1a6e41a1bfdfdc5d38
Christian Hellum Bye <christian.hellum.bye@gmail.com>
2021-12-05 18:50:34 -0800

    finish pt2

--- dec3/solution.py
+++ dec3/solution.py
@@ -13,7 +13,42 @@ def split_digits(arr):
         newarr[i, x:]=newrow
     return newarr
 
-tot=np.sum(split_digits(report), axis=0)[::-1]
+split_report=split_digits(report)
+
+# part 1
+tot=np.sum(split_report, axis=0)[::-1]
 gamma=np.sum(2**(np.argwhere(tot>n/2)[:, 0]))
 epsilon=np.sum(2**(np.argwhere(tot<n/2)[:, 0]))
-print(gamma*epsilon)
+with open('output.txt', 'a+') as f:
+    f.write('Part 1: {:d}\n'.format(gamma*epsilon))
+
+# part 2
+def filter(arr, bit_no, rating='ogr'):
+    n=len(arr)
+    if n==1:
+        return arr
+    else:
+        if rating=='ogr':
+            condition=int(np.sum(arr[:, bit_no])>=n/2)
+        elif rating=='csr':
+            condition=int(np.sum(arr[:, bit_no])<n/2)
+        ind=np.argwhere(arr[:, bit_no] == condition)[:, 0]
+        return arr[ind]
+
+x, y = 0, 0
+for i in range(m):
+    if i==0:
+        ogarr=split_report
+        csarr=split_report
+    else:
+        ogarr=x
+        csarr=y
+    x=filter(ogarr, i, rating='ogr')
+    y=filter(csarr, i, rating='csr')
+
+ogr=np.sum(2**np.argwhere(x[0, ::-1]==1)[:, 0])
+csr=np.sum(2**np.argwhere(y[0, ::-1]==1)[:, 0])
+
+with open('output.txt', 'a') as f:
+    f.write('Part 2: {:d} \n'.format(ogr*csr))
+    
